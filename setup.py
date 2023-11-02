60ae479b8e1c08dc8bcd1b56bdf60a7e9f069a86
JosephVankelegom <jtevanvan@gmail.com>
2023-11-02 23:00:04 +0100

    First commit

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="inginious-problems-demo",
+    name="inginious-problems-sublabel",
     version="0.1dev0",
-    description="Plugin to add demo problem type",
+    description="Plugin to add sublabel problem type",
     packages=find_packages(),
     install_requires=["inginious>=0.5.dev0"],
     tests_require=[],
     extras_require={},
     scripts=[],
     include_package_data=True,
-    author="The INGInious authors",
+    author="Joseph Vankelegom",
     author_email="inginious@info.ucl.ac.be",
     license="AGPL 3",
     url="https://github.com/UCL-INGI/INGInious-problems-demo"
