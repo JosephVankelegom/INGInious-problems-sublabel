38643d3533088a6808a204fc55ebf44bef48b2f8
JosephVankelegom <jtevanvan@gmail.com>
2023-11-13 02:13:00 +0100

    selection working, still need to check why it's not working on the first textarea.

--- inginious_problems_sublabel/static/sublabel.js
+++ inginious_problems_sublabel/static/sublabel.js
@@ -1,3 +1,5 @@
+
+
 function load_input_sublabel(submissionid, key, input) {
     var field = $("form#task input[name='" + key + "']");
     if(key in input)
@@ -16,12 +18,46 @@ function load_feedback_sublabel(key, content) {
     load_feedback_code(key, content);
 }
 
+
+$(document).on('mouseup','textarea',function(){
+    var selectedText = getSelectionText(this);
+    $("#debug").html("Selection : '" + selectedText + "'");
+})
+
+function getSelectionText(textarea){
+    if(window.getSelection){
+        try{
+            return textarea.value.substring(textarea.selectionStart, textarea.selectionEnd);
+        }
+        catch (e)
+        {
+            console.log('Cant get seletion text ERROR')
+        }
+    }
+    if(document.selection && document.selection.type !== "Control") {
+        return document.selection.createRange().text;
+    }
+}
 function highlight(){
     var selection = window.getSelection();
+    console.log(selection.toString());
+    document.body.style.background = "green";
+
     if (selection > 0){
         var range = selection.getRangeAt(0);
         var span = document.createElement("span");
         span.className = "highlight" ;
         range.surroundContents();
     }
+}
+
+function selectionUpdate(){
+    HTMLInputElement().select();
+
+}
+
+function myFunction(){
+    var input = getSelectionText()
+    var output = document.getElementById('output')
+    output.value = input.value;
 }
